29564ab7c997b56a603f2f7ae1a0f702196fe052
jackcornellober <jackcornellober@gmail.com>
2019-05-07 15:32:15 -0700

    array-methods.js and closure.js complete. moving on to stretch goals.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -1,4 +1,5 @@
-// A local community center is holding a fund raising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
+// A local community center is holding a fund raising 5k fun run and has invited 50 small businesses to make a small donation on their behalf 
+// for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
 
 // Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
 
@@ -54,30 +55,52 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.
+// Combine both the first and last names into a new array called fullName. 
+
 let fullName = [];
+runners.forEach(runner => fullName.push(`${runner.first_name} ${runner.last_name}`))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+
+let capMap = runners.map(runner => runner.first_name.toUpperCase())
+console.log(capMap); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+
+let largeShirts = runners.filter(runner => runner.shirt_size == "L")
+console.log(largeShirts)
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+//const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+let ticketPriceTotal = runners.reduce((donation, runner) => donation + runner.donation, 0)
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+    //The event director wants to know which companies donated more than $200. Make it so.
+
+let philanthropists = runners.filter(runner => runner.donation > 199).map(runner => runner.company_name)
+console.log(philanthropists)
 
 // Problem 2
+    //The event director wants a list of all of the emails separated by commas in one string so that he can easily send them all an email. Do it up.
+
+    let emails = runners.reduce((email, runner) => email + runner.email + ", ", "")
+    console.log(emails)
 
 // Problem 3
+    //The event director has gone home for the night. It's just you in the office now. Shave $10 from each donation and keep it all for yourself.
+
+let ibizaVacationFund = 0
+let totallyLegitRunnersArrayNotTamperedWithAtAll = runners.map(runner => {runner.donation = runner.donation -10; ibizaVacationFund = ibizaVacationFund + 10; return runner;})
+console.log(totallyLegitRunnersArrayNotTamperedWithAtAll)
+console.log("Ibiza Vacation Fund: $" + ibizaVacationFund)
+
